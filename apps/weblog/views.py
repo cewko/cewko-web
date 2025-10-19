434fb021566204648a1d42263da7652842d33a2a
cewko <221901489+cewko@users.noreply.github.com>
2025-10-19 21:25:14 +0200

    style(weblog): redesign article page

--- apps/weblog/views.py
+++ apps/weblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.cache import cache_page
 from apps.weblog.models import Article, Comment
 
@@ -18,7 +18,10 @@ def article_page(request, slug):
     comments = article.comments.all()
 
     if request.method == "POST":
-        nickname = request.POST.get("nickname")[:64]
+        nickname = request.POST.get("nickname", "anonymous")[:64].strip()
+        if not nickname:
+            nickname = "anonymous"
+        
         body = request.POST.get("body", "").strip()
 
         if body:
@@ -27,10 +30,9 @@ def article_page(request, slug):
                 nickname=nickname,
                 body=body
             )
+            return redirect('weblog:article_page', slug=slug)
 
     return render(request, "weblog/article_page.html", {
         "article": article,
         "comments": comments
     })
-
-
